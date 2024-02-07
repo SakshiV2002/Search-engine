0b0212f69094519469d96fbc7373cad9ccc552ea
SakshiV2002 <159016344+SakshiV2002@users.noreply.github.com>
2024-02-07 09:32:29 +0530

    Add files via upload

---
+++ imagesearch/script.js
@@ -0,0 +1,50 @@
+
+const accessKey="sixQ4Fpr75kZw0P1uSwJ8v1620NVGf0kJAW8MH3ACh8";
+
+
+const searchForm=document.getElementById("search-form");
+const searchBox=document.getElementById("search-box");
+const searchResult=document.getElementById("search-result");
+const showmoreBtn=document.getElementById("show-more-btn");
+
+let keyword="";
+let page=1;
+
+async function searchImages(){
+   keyword=searchBox.value;
+   const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+   const response=await fetch(url);
+   const data=await response.json();
+
+   if(page===1){
+    searchResult.innerHTML="";
+   }
+   
+   const results=data.results;
+   results.map((results) =>{
+    const image=document.createElement("img");
+    image.src=results.urls.small;
+    const imageLink=document.createElement("a");
+    imageLink.href=results.links.html;
+    imageLink.target="_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+   })
+   showmoreBtn.style.display="block";
+
+}
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page=1;
+    searchImages();
+
+})
+showmoreBtn.addEventListener("click", ()=>{
+    page++;
+    searchImages();
+
+
+}
+)
